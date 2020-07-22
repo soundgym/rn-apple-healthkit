a3cb5540056018fdf668bae0e144980ef92721ef
bang9dev <gusrn1423@naver.com>
2020-07-22 14:15:25 +0900

    fix: prettier

--- Constants/index.d.ts
+++ Constants/index.d.ts
@@ -1,4 +1,4 @@
-import Activities from './Activities';
-import Permissions from './Permissions';
-import Units from './Units';
+import Activities from "./Activities";
+import Permissions from "./Permissions";
+import Units from "./Units";
 export { Activities, Permissions, Units };
--- Constants/index.ts
+++ Constants/index.ts
@@ -1,5 +1,5 @@
-import Activities from './Activities'
-import Permissions from './Permissions'
-import Units from './Units'
+import Activities from "./Activities";
+import Permissions from "./Permissions";
+import Units from "./Units";
 
-export { Activities, Permissions, Units }
+export { Activities, Permissions, Units };
